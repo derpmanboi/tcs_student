707e047ccb1687e3e28af3a67e44ac616ec1bcc3
derpmanboi <skibidibartholemewjrthe3rd@gmail.com>
2025-02-22 09:59:52 -0800

    fffff

--- Martin Leung/Bullet/Assets/scripts/xiaohongshu.cs
+++ Martin Leung/Bullet/Assets/scripts/xiaohongshu.cs
@@ -6,7 +6,7 @@ public class xiaohongshu : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        xia0hongshu
+
     }
 
     // Update is called once per frame
